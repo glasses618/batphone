3aa57e2ffdee978df010af6b2b5c413e7a81fa4f
Shih Ying <glasses618@gmail.com>
2017-03-17 14:43:29 +0800

    add SecertKey and SecretController with tests

---
+++ src/org/servalproject/group/secret/SecretController.java
@@ -0,0 +1,94 @@
+package org.servalproject.group.secret;
+
+import com.tiemens.secretshare.engine.SecretShare;
+import com.tiemens.secretshare.math.BigIntUtilities;
+import com.tiemens.secretshare.exceptions.SecretShareException;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.UnsupportedEncodingException;
+
+public class SecretController {
+
+    private SecretKey mMasterkey;
+    private SecretShare.PublicInfo mPublicInfo;
+    private ArrayList<SecretKey> mSubkeyList;
+    
+    public SecretController(){
+    
+    }
+    
+    public ArrayList<SecretKey> generateSubkeyList(Integer n, Integer k) {
+        mSubkeyList = new ArrayList<SecretKey>();
+        if (masterKeyExist()) {
+            mPublicInfo = new SecretShare.PublicInfo(
+                n, k, SecretShare.getPrimeUsedFor4096bigSecretPayload(), "");
+            SecretShare ss = new SecretShare(mPublicInfo);
+            SecretShare.SplitSecretOutput out = ss.split(mMasterkey.getKeyBigInteger());
+            List<SecretShare.ShareInfo> shareInfo = out.getShareInfos();
+            for (SecretShare.ShareInfo share: shareInfo) {
+                mSubkeyList.add(new SecretKey(share.getShare()));
+            }
+        }
+        return mSubkeyList;
+    }
+
+    public SecretKey reconstructMasterkey(
+            ArrayList<SecretKey> subkeyList) {
+
+        SecretKey masterkey = new SecretKey("");
+        SecretShare solver = new SecretShare(mPublicInfo);
+        ArrayList<SecretShare.ShareInfo> shareInfoList =
+            new ArrayList<SecretShare.ShareInfo>();
+        for (int i = 0; i < subkeyList.size(); i++) {
+            shareInfoList.add(new SecretShare.ShareInfo(i+1, subkeyList.get(i).getKeyBigInteger(), mPublicInfo));
+        }
+        try {
+            SecretShare.CombineOutput solved = solver.combine(shareInfoList);
+            masterkey = new SecretKey(solved.getSecret());
+        } catch (SecretShareException e) {
+            System.out.println("Not enough keys"); 
+        }
+        mMasterkey = masterkey;
+        return masterkey;
+    }
+
+    public ArrayList<SecretKey> updateSubkeyList() {
+
+        ArrayList<SecretKey> keyList =
+            generateSubkeyList(mPublicInfo.getN(), mPublicInfo.getK());
+        return keyList;
+    }
+
+    public ArrayList<SecretKey> getSubkeyList() {
+        return mSubkeyList;
+    }
+
+    public SecretKey getMasterkey() {
+        return mMasterkey;
+    }
+    public void generateMasterkey() {
+        SecureRandom random = new SecureRandom();
+        mMasterkey = new SecretKey(new BigInteger(130, random).toString(32));
+
+    }
+
+    public boolean masterKeyExist() {
+        return (mMasterkey != null) ? true : false;
+    }
+
+    public String encryptData(String input, SecretKey key) {
+
+        StringBuffer output = new StringBuffer();
+        String keyString = key.getKey(); 
+        for (int i = 0; i< input.length(); i++) {
+            output.append((char) (input.charAt(i) ^ keyString.charAt(i % keyString.length())));
+        }
+
+        return output.toString();
+    }
+    public String decryptData(String input, SecretKey key) {
+        return encryptData(input, key);
+    }
+}
---
+++ test/org/servalproject/group/secret/SecretControllerTest.java
@@ -0,0 +1,54 @@
+import org.servalproject.group.secret.SecretController;
+import org.servalproject.group.secret.SecretKey;
+import com.tiemens.secretshare.engine.SecretShare;
+import com.tiemens.secretshare.math.BigIntUtilities;
+import com.tiemens.secretshare.exceptions.SecretShareException;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SecretControllerTest {
+    SecretController secretController = new SecretController();
+    SecretKey masterkey = new SecretKey("");
+    ArrayList<SecretKey> subkeyList = new ArrayList<SecretKey>();
+    @Test   
+    public void keyTest() {
+        secretController.generateMasterkey();
+        masterkey = secretController.getMasterkey();
+        System.out.println(masterkey.getKey());
+        subkeyList = secretController.generateSubkeyList(5, 3);
+        for (SecretKey key: subkeyList) {
+            System.out.println(key.getKey());
+        }
+        SecretKey reconstructedkey = secretController.reconstructMasterkey(subkeyList);
+        System.out.println(reconstructedkey.getKey());
+        subkeyList = secretController.updateSubkeyList();
+        for (SecretKey key: subkeyList) {
+            System.out.println(key.getKey());
+        }
+        reconstructedkey = secretController.reconstructMasterkey(subkeyList);
+        System.out.println(reconstructedkey.getKey());
+
+        ArrayList<SecretKey> enoughSubkeys = 
+            new ArrayList<SecretKey>(subkeyList.subList(0,3));
+        reconstructedkey = secretController.reconstructMasterkey(enoughSubkeys);
+        System.out.println(reconstructedkey.getKey());
+
+        ArrayList<SecretKey> notEnoughSubkeys = 
+            new ArrayList<SecretKey>(subkeyList.subList(0,1));
+        reconstructedkey = secretController.reconstructMasterkey(notEnoughSubkeys);
+        System.out.println(reconstructedkey.getKey());
+        String oriData = "hello, world 我是秘密哈哈!@#&(*&(*)(#";
+        System.out.println(oriData);
+        String enData =  secretController.encryptData(oriData, masterkey);
+        System.out.println(enData);
+        String data =  secretController.decryptData(enData, masterkey);
+        System.out.println(data);
+    }
+
+}
+
